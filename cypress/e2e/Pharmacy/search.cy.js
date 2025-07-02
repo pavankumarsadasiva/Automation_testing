4c53b651814ed63f1cbba0f87e997eed7d97ca23
pavankumarsadasiva <pavans5699@gmail.com>
2025-07-02 16:22:29 +0530

    changes

--- cypress/e2e/Pharmacy/search.cy.js
+++ cypress/e2e/Pharmacy/search.cy.js
@@ -4,7 +4,7 @@ describe('Pharmacy Search', () => {
     cy.wait(2000); // Wait for the page to load
     cy.get('.search-input').type('Dolo 650');
      // Wait for search results to load
-    cy.get('.search-popup',{timeout:12000}).should('contain', 'Dolo 650 Tablet');
+    cy.get('.search-popup',{timeout:15000}).should('contain', 'Dolo 650 Tablet');
 
   });
   
