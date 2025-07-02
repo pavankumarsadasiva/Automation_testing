9bbd425664b342b439c6f96056f5f28e32de8973
pavankumarsadasiva <pavans5699@gmail.com>
2025-07-02 11:38:41 +0530

    change

--- cypress/e2e/Doctor_consultation/DoctorConsultationSearch.cy.js
+++ cypress/e2e/Doctor_consultation/DoctorConsultationSearch.cy.js
@@ -60,10 +60,10 @@ describe('Doctor Consultation Search and Booking', () => {
               cy.wrap($buttons[randomIndex]).click({force: true});
             });
             cy.wait(5000)  // optional
-            cy.get('.pay-proceed', { timeout: 10000 }).should('be.visible').click()
+            // cy.get('.pay-proceed', { timeout: 10000 }).should('be.visible').click()
 
           // Step 6: Proceed to Checkout
-          // cy.get('.pay-proceed').should('be.visible').click();
+          cy.get('.pay-proceed').should('be.visible').click();
           
           cy.url().should('include', '/checkout');
           // Optional: PhonePe payment test (mock or conditionally check)
